5491f600e7855fbc9d0b5a174caabf8f629cbfd4
Tsvetan <tsvetan.y.petrov@gmail.com>
2018-12-30 07:40:49 +0200

    Sesting additional dev

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 
 app.use((request, response, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${request.method} ${request.url}`;
-  console.log(log);
-  fs.appendFile("server.log", log + "\n", err => {
-    if (err) {
-      console.log("Unable to append to file.");
-    }
-  });
-  next();
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} ${request.url}`;
+    console.log(log);
+    fs.appendFile("server.log", log + "\n", err => {
+        if (err) {
+            console.log("Unable to append to file.");
+        }
+    });
+    next();
 });
 
 // app.use((request, response, next) => {
@@ -27,27 +27,33 @@ app.use((request, response, next) => {
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper("getCurrentYear", () => {
-  return new Date().getFullYear();
+    return new Date().getFullYear();
 });
 
 app.get("/", (request, response) => {
-  response.render("home.hbs", {
-    page_title: "Home page"
-  });
+    response.render("home.hbs", {
+        page_title: "Home page"
+    });
 });
 
 app.get("/about", (request, response) => {
-  response.render("about.hbs", {
-    page_title: "About page"
-  });
+    response.render("about.hbs", {
+        page_title: "About page"
+    });
+});
+
+app.get("/projects", (request, response) => {
+    response.render("projects.hbs", {
+        page_title: "Projects page"
+    });
 });
 
 app.get("/bad", (request, response) => {
-  response.send({
-    errorMessage: "Page not found"
-  });
+    response.send({
+        errorMessage: "Page not found"
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+    console.log(`Server is up on port ${port}`);
 });
